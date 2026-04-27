007260d295c95e59ab546987ea7c5f24efe5a965
Hampus Berg <hampus.berg@pm.me>
2026-04-27 14:57:34 +0200

    Added logic to add header when creating a csv, plus helper functions; write_row and close.

--- scripts/download.py
+++ scripts/download.py
@@ -5,16 +5,49 @@ BASE_DIR = '../raw_data'
 METADATA_DIR = os.path.join(BASE_DIR, 'metadata')
 STUDIES_FILE = os.path.join(METADATA_DIR, 'studies.csv')
 
+
 class Csv:
-    def __init__(self, filepath):
+    """This class handels csv files.
+
+    The class makes the folder path if it dose not exist. It opens the
+    csv file in append mode.
+
+    Args:
+        filepath (str): File path to where the csv will be created or
+           opened.
+        header (list[str]): A list of string used as headers in the csv
+         file.
+    """
+    def __init__(self, filepath: str, header: list[str] = None):
+        self.filepath = filepath
+
+        # Ensure parent directory exists
+        os.makedirs(METADATA_DIR, exist_ok=True)
+
+        write_header = False
+
         if not os.path.isfile(filepath):
-            os.makedirs(METADATA_DIR)
-            
-        self.csv = open(filepath, 'a', newline='')
-        self.writer = csv.writer(self.csv, delimiter=' ')
+            write_header = True
+        elif os.stat(filepath).st_size == 0:
+            write_header = True
+
+        self.f = open(filepath, 'a')
+        self.writer = csv.writer(self.f)
+
+        if write_header and header:
+            self.writer.writerow(header)
+
+    def write_row(self, row_data):
+        self.writerow(row_data)
+        self.f.flush()
+
+    def close(self):
+        self.f.close()
 
 def main():
-    studies = Csv(STUDIES_FILE)
+    studies = Csv(STUDIES_FILE, ['id', 'uuid', 'title'])
+
+    studies.close()
 
 if __name__ == "__main__":
     main()
