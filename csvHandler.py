d085e650e6b7569e59c81ce280c360a58e2e5c1b
Hampus Berg <hampus.berg@pm.me>
2026-05-18 13:28:31 +0200

    changed module os to pathlib

--- csvHandler.py
+++ csvHandler.py
@@ -1,42 +1,37 @@
-import os
 import csv
+from pathlib import Path
 
 class CsvHandler:
-    """This class handels csv files.
+  """This class handels csv files.
 
-    The class makes the folder path if it dose not exist. It opens the
-    csv file in append mode.
+  The class makes the folder path if it dose not exist. It opens the
+  csv file in append mode.
 
-    Args:
-        filepath (str): File path to where the csv will be created or
-           opened.
-        header (list[str]): A list of string used as headers in the csv
-         file.
-    """
-    def __init__(self, filepath: str, header: list[str] = None):
-        self.filepath = filepath
+  Args:
+    filepath (str): File path to where the csv will be created or
+      opened.
+    header (list[str]): A list of string used as headers in the csv
+      file.
+  """
+  def __init__(self, filepath: str, header: list[str] = None):
+    self.filepath = Path(filepath)
+    self.header = header
+    self.write_header = False
 
-        write_header = False
+    if not self.filepath.exists:
+      write_header = True
 
-        if not os.path.isfile(filepath):
-            write_header = True
-        elif os.stat(filepath).st_size == 0:
-            write_header = True
+    Path(self.filepath).parent.mkdir(parents=True, exist_ok=True)
 
-        self.f = open(filepath, 'a')
-        self.writer = csv.writer(self.f)
+    self.f = open(self.filepath, 'a+')
+    self.writer = csv.writer(self.f)
 
-        if write_header and header:
-            self.writer.writerow(header)
+    if self.write_header and self.header:
+      self.writer.writerow(self.header)
 
-    def write_row(self, row_data):
-        self.writerow(row_data)
-        self.f.flush()
+  def write_row(self, row_data):
+    self.writerow(row_data)
+    self.f.flush()
 
-    def close(self):
-        self.f.close()
-
-def main():
-
-if __name__ == "__main__":
-    main()
+  def close(self):
+    self.f.close()
