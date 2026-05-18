6e92d857d52af1cf3bc070dfd41fa60f4d0b2b5c
Hampus Berg <hampus.berg@pm.me>
2026-05-18 15:31:05 +0200

    added regex

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 from pathlib import Path
 
+# Paths
 RESULT_INDEX = Path('sourcedata/JATOS/raw_data/result_index.csv')
+
+# Lists
 RESULT_INDEX_HEADERS = [
   'result_id', 'result_uuid', 'study_id', 'study_uuid', 'start_date',
   'end_date', 'study_state', 'url_query_parameters', 'downloaded_at',
   'download_status',
 ]
+
+# Regex
+ REGEX_TITLE = r'^[^_]+'
