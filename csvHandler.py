ceabd08f73b2c3a4c7e4e1fc0aec76fc2bdc5a9d
Hampus Berg <hampus.berg@pm.me>
2026-05-22 14:37:50 +0200

    Added a function to get column data from csv

--- csvHandler.py
+++ csvHandler.py
@@ -32,5 +32,24 @@ class CsvHandler:
     self.writer.writerow(row_data)
     self.f.flush()
 
+  def get_column(self, headers: str | list[str]) -> list[str] | list[list[str]]:
+    self.f.seek(0) # Set the reader at the beginning of the file.
+    reader = csv.DictReader(self.f)
+
+    if isinstance(headers, str):
+      headers = [headers]
+
+    columns_data = [[] for _ in headers]
+
+    for row in reader:
+      for i, header in enumerate(headers):
+        columns_data[i].append(row.get(header))
+
+    self.f.seek(0,2) # Set the reader at the end of the file.
+    if len(columns_data) == 1:
+      return columns_data[0]
+
+    return columns_data
+
   def close(self):
     self.f.close()
