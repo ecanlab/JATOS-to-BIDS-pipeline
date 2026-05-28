cdc0ae47e408578c3de5943139a96605c709ccda
Hampus Berg <hampus.berg@pm.me>
2026-05-28 15:34:22 +0200

    Updated docstring

--- utils.py
+++ utils.py
@@ -2,34 +2,47 @@ import re
 import config
 import datetime
 
-def get_part_of_string(s: str, regex: str) -> str:
+def get_part_of_string(text: str, regex: str) -> str:
   '''
   Get the matching regex from a string.
-  PRE: regex should be a raw string, r''
-  ARGS: study_title (str): The study title that contains the project name.
-        regex       (str): Raw string to match against the study_title.
-  RETURNS: The matching string.
+
+  PRE:
+    regex should be a raw string, r''
+
+  ARGS:
+    text (str): The string that will be searched.
+    regex (str): Raw string regex pattern to match against the text.
+
+  RETURNS:
+    The matching string or an empty string if nothing was found.
   '''
-  result = re.search(regex, s)
+  result = re.search(regex, text)
 
   if result:
     return result.group(0)
   else:
     return ''
 
-def convert_to_local_tz(date: float) -> str:
+def convert_to_local_tz(date_ms: float) -> str:
   '''
-  Convert an int to local timezone with the format (YYYY-MM-DD HH-mm-ss)
-  PRE: date must be a date reprecented as miliseconds.
-  ARGS: date (str): A date reprecented as miliseconds.
-  RETURN: A datetime.datetime object with the local timezone.
+  Convert an float to local timezone with the format (YYYY-MM-DD HH-mm-ss)
+
+  PRE:
+    date must be a date reprecented as miliseconds.
+
+  ARGS:
+    date_ms (float): A date reprecented as miliseconds.
+
+  RETURNS:
+    The time with the format (YYYY-MM-DD HH-mm-ss) converted to the local
+    timezone.
   '''
   # Convert the date in ms to UTC format
   start_date_utc = datetime.datetime.fromtimestamp(
-    date / 1000.0,
+    date_ms / 1000.0,
     tz=datetime.timezone.utc
   )
-  # Convert UTC to local time
+  # Convert to local time
   date_local_tz = start_date_utc.astimezone(config.LOCAL_TZ)
 
   # Apply format
