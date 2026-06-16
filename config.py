2084f44385501cebc922fd20845695db4c0c18c0
Hampus Berg <hampus.berg@pm.me>
2026-06-16 15:58:25 +0200

    Added regex and paths.

--- config.py
+++ config.py
@@ -5,7 +5,10 @@ from pathlib import Path
 JATOS_FOLDER = Path('sourcedata/JATOS')
 RAW_DATA     = Path('sourcedata/JATOS/raw_data/')
 RESULT_INDEX = Path(RAW_DATA / 'result_index.csv')
+NORMALIZED_DATA = Path('sourcedata/JATOS/normalized_data')
+SCHEMA_MAP = Path('code/JATOS/schema_map.json')
 DOWNLOAD_LOG = Path('code/JATOS/logs/download.log')
+NORMALIZE_LOG = Path('code/JATOS/logs/normalize.log')
 
 # Download states
 DOWNLOADED = 'downloaded'
@@ -23,6 +26,8 @@ REGEX_PROJECT_TITLE  = r'^[^_]+'
 REGEX_PROJECT_ARM    = r'_arm-[\d]+'
 REGEX_PROJECT_SES    = r'_ses-[\d]+'
 REGEX_PROJECT_TASK   = r'_task-[\w]+'
+REGEX_TASK_NAME      = r'^(?:(?!\bv\.\d+(?:\.\d+)*\b).)*'
+REGEX_TASK_VERSION   = r'v\.\d+(?:\.\d+)*'
 
 # Time
 TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
