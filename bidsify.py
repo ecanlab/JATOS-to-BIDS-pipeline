34002e8b0b0b9eaedb5488581d583569031d8702
Hampus Berg <hampus.berg@pm.me>
2026-08-26 12:09:57 +0200

    Added command argument -p/--projects so the user can specify projects to BIDSify and skip the rest.

--- bidsify.py
+++ bidsify.py
@@ -1,4 +1,5 @@
 # Standard libray
+import argparse
 import json
 import logging
 import os
@@ -18,11 +19,39 @@ import config
 import log as log_util
 import utils
 
+def get_args() -> argparse.Namespace:
+  """Parses the command-line arguments.
+
+  Returns: argparse.Namespace: Command-line arguments inputs as an
+    argparse.Namespace object.
+  """
+  parser = argparse.ArgumentParser(
+    prog='BIDSify',
+    description='BIDSify data from JATOS, validated by the validation script'
+  )
+
+  parser.add_argument(
+    '-p',
+    '--projects',
+    nargs='*',
+    help='one or more project to BIDSify'
+  )
+
+  args = parser.parse_args()
+
+  return args
+
 class BIDSifier():
-  def __init__ (self, project_root: str, log: logging.Logger):
+  def __init__ (
+      self,
+      project_root: str,
+      args: argparse.Namespace,
+      log: logging.Logger
+  ):
     self.project_root = Path(project_root)
-    self.log = log
-    self.configs = ConfigLoader(project_root / config.TASK_CONFIGS)
+    self.args         = args
+    self.log          = log
+    self.configs      = ConfigLoader(project_root / config.TASK_CONFIGS)
 
   def run(self):
     project_dirs = utils.get_all_project_dirs(self.project_root)
@@ -33,6 +62,9 @@ class BIDSifier():
       sys.exit(1)
 
     for project_dir in project_dirs:
+      # Skip project if is not specified by the user
+      if self.args.projects and project_dir.name not in self.args.projects:
+        continue
 
       path_task_config = (
         self.project_root /
@@ -42,20 +74,28 @@ class BIDSifier():
 
       path_validated_data = project_dir / config.VALIDATED_DATA
 
+      files = [
+        f for f in path_validated_data.glob('*.tsv')
+        if f.name != config.VALIDATION_PROTOCOL.name
+      ]
+
+      total = len(files)
+      if total <= 1:
+        self.log.critical(
+          'Did not find any result files in %s, run validation.py first',
+          project_dir.name / config.VALIDATED_DATA
+        )
+        continue
+
       self.log.info(
         'Copying data from %s to %s root and structuring it into BIDS',
         path_validated_data.stem,
         project_dir.name
       )
 
-      files = [
-        f for f in path_validated_data.glob('*.tsv')
-        if f.name != config.VALIDATION_PROTOCOL.name
-      ]
-
       pbar = tqdm(
         files,
-        total=len(files),
+        total=total,
         desc=f'BIDSifying {project_dir.name}: ',
         unit=' files'
       )
@@ -105,6 +145,9 @@ class BIDSifier():
     self.log.info('BIDSification completed')
 
 if __name__ == "__main__":
+  """Load configuration and start the BIDSifyer."""
+  args = get_args()
+
   load_dotenv()
   project_root = os.getenv('project_root')
 
@@ -115,5 +158,5 @@ if __name__ == "__main__":
   log = log_util.setupLogging(project_root / config.VALIDATE_LOG)
   log.info('Configuration loaded successfully')
 
-  bidsifier = BIDSifier(project_root, log)
+  bidsifier = BIDSifier(project_root, args, log)
   bidsifier.run()
